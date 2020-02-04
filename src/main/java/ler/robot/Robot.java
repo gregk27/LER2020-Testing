99633d61035caec0554458d6eff3d2c431e974aa
Aree-Vanier <git@gregk.ca>
2020-02-04 08:28:16 -0500

    Separated OI into own class, should help make code cleaner

--- src/main/java/ler/robot/Robot.java
+++ src/main/java/ler/robot/Robot.java
@@ -23,6 +23,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  public static OI oi = new OI();
 
   /**
    * This function is run when the robot is first started up and should be used for any
