cb19dd5a3d33f376a52048efebf9c5d55f678340
KBotics <k.botics@gmail.com>
2020-02-22 11:13:21 -0500

    Drive works we think, something screwy with right drive gearbox. Further testing needed.

--- src/main/java/ler/robot/subsystems/Shooter.java
+++ src/main/java/ler/robot/subsystems/Shooter.java
@@ -7,10 +7,11 @@
 
 package ler.robot.subsystems;
 
-//import com.ctre.phoenix.motorcontrol.ControlMode;
-//import com.revrobotics.CANPIDController;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.ControlType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import ler.robot.RobotMap;
 
@@ -21,9 +22,9 @@ public class Shooter extends SubsystemBase{
   public static final double kD = 0.000001;
   public static final double kF = 0.000086;
 
-  private static final double SPIN_CONSTANT = 0.85;
+  private static final double SPIN_CONSTANT = 1.3;
 
-  public static final double[] SPEEDS = {0, 0.9};
+  public static final double[] SPEEDS = {0, 0.8};
   public static final int ZEROSPEED = 0;
   public static final double limelightSpeedScaling = 0.01;
 
@@ -43,28 +44,29 @@ public class Shooter extends SubsystemBase{
   }
 
   public void setSpecificShooterSpeed(double speed){
-    //talonShooterTop.set(ControlMode.PercentOutput, speeds[currentSpeed]);
-    //System.out.println(RobotMap.shooterTopTalon.contr);
+    if(speed == 0) {
+      RobotMap.shooterTopSpark.set(0);
+      RobotMap.shooterBottomSpark.set(0);
+    }
+    else {
+      
+      //TODO: Debbuging, change to constant when tuned
     RobotMap.shooterTopSpark.getPIDController().setReference((speed*SPIN_CONSTANT), ControlType.kVelocity);
-    //talonShooterBottom.set(ControlMode.PercentOutput, -speeds[currentSpeed]);
+
     RobotMap.shooterBottomSpark.getPIDController().setReference(-speed, ControlType.kVelocity);
+    }
   }
 
-  public void talonResetPos(){
-    RobotMap.shooterTopSpark.getEncoder().getPosition();
-    RobotMap.shooterTopSpark.getEncoder().getPosition();
-  }
-
-  public double getTopTalonSpeed(){
+  public double getTopSparkSpeed(){
     return(RobotMap.shooterTopSpark.getEncoder().getVelocity());
   }
 
-  public double getBottomTalonSpeed(){
-    return(RobotMap.shooterTopSpark.getEncoder().getPosition());
+  public double getBottomSparkSpeed(){
+    return(RobotMap.shooterBottomSpark.getEncoder().getVelocity());
   }
 
-  public double getAverageTalonSpeed(){
-    return((getTopTalonSpeed() + getBottomTalonSpeed()) / 2);
+  public double getAverageSparkSpeed(){
+    return((getTopSparkSpeed() + getBottomSparkSpeed()) / 2);
   }
 
   //speed should be how far the bot can shoot straight up
