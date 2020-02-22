16067104aafa706e6ef13f669d6c5b221f31248f
Halil Sina Kelebek <halilsina.kelebek@gmail.com>
2020-02-22 16:23:15 -0500

    Made InvertControlsCommand an instant command

--- src/main/java/ler/robot/commands/instant/InvertControlsCommand.java
+++ src/main/java/ler/robot/commands/instant/InvertControlsCommand.java
@@ -5,12 +5,12 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package ler.robot.commands;
+package ler.robot.commands.instant;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import ler.robot.subsystems.Drivetrain;
 
-public class InvertControlsCommand extends CommandBase {
+public class InvertControlsCommand extends InstantCommand {
   Drivetrain drivetrain;
   /**
    * Creates a new invertControlsCommand.
@@ -26,20 +26,9 @@ public class InvertControlsCommand extends CommandBase {
     drivetrain.invertControls();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    
-  }
-
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
   }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return true;
-  }
 }
