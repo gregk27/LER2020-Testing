b000f4a2f55ffe80c3cdc903fe57899fdfc85570
Glenn Buckholtz <buckholt@queensu.ca>
2020-03-05 09:58:26 -0500

    This is an initial port of Tim's auto gyro code

--- src/main/java/ler/robot/commands/autonomous/GyroTurnCommand.java
+++ src/main/java/ler/robot/commands/autonomous/GyroTurnCommand.java
@@ -1,9 +1,16 @@
 package ler.robot.commands.autonomous;
 
+import static ler.robot.Tools.closestEquivalentAngle;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import ler.robot.Robot;
+import ler.robot.subsystems.Drivetrain;
+import ler.robot.subsystems.Gyro;
 
 public class GyroTurnCommand extends CommandBase {
+
+	private Drivetrain drivetrain;
+	private Gyro gyro;
 	private double target_angle;
 	private boolean finished = false;
 	private boolean absolute;
@@ -14,33 +21,41 @@ public class GyroTurnCommand extends CommandBase {
 	final double TOLERANCE = 15;
 	final double GYRO_MULTIPLIER = 5.475d / 360d;
 	boolean clockwise = false;
+
 	/**
-	 * Turns to an angle in degrees. Positive is CCW. If absolute is true, it turns to the angle relative to the angle it was at when enabled.
-	 *@author Tim
+	 * Turns to an angle in degrees. Positive is CCW. If absolute is true, it turns
+	 * to the angle relative to the angle it was at when enabled.
+	 * 
+	 * @author Tim
 	 */
-    public GyroTurnCommand(double target_angle, boolean absolute) {
-    	requires(Robot.Drivetrain);
-    	setTimeout(Robot.AUTO_TIMEOUT);
-    	this.absolute = absolute;
-    	this.target_angle = target_angle;
-    }
+	public GyroTurnCommand(Drivetrain drivetrain, Gyro gyro, double target_angle, boolean absolute) {
+		this.drivetrain = drivetrain;
+		this.gyro = gyro;
+		addRequirements(this.drivetrain);
+		addRequirements(this.gyro);
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	if (!absolute) {
-    		Robot.gyro.resetAngle();
-//        	target_angle -= target_angle * GYRO_MULTIPLIER; // gyro tends to increase in error as it turns, this compensates (somewhat)
-    		clockwise = target_angle < 0; // gyro tends to increase in error as it turns, this compensates (somewhat)
-    	}
-    	else {
-    		target_angle = Tools.closestEquivalentAngle(target_angle);
+		this.absolute = absolute;
+		this.target_angle = target_angle;
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	public void initialize() {
+		if (!absolute) {
+			gyro.resetAngle();
+			// target_angle -= target_angle * GYRO_MULTIPLIER; // gyro tends to increase in
+			// error as it turns, this compensates (somewhat)
+			clockwise = target_angle < 0; // gyro tends to increase in error as it turns, this compensates (somewhat)
+		} else {
+			target_angle = closestEquivalentAngle(gyro, target_angle);
 //    		target_angle += (target_angle - Robot.gyro.getAbsoluteAngle()) * GYRO_MULTIPLIER; // gyro tends to increase in error as it turns, this compensates (somewhat)	
-    		clockwise = Robot.gyro.getAbsoluteAngle() > target_angle;
+    		clockwise = gyro.getAbsoluteAngle() > target_angle;
     	}
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+    public void execute() {
     	double l = 0;
     	double r = 0;
     	double calculated_speed = 0;
@@ -48,10 +63,10 @@ public class GyroTurnCommand extends CommandBase {
     	double difference;
 
     	if (absolute) {
-    		difference = target_angle - Robot.gyro.getAbsoluteAngle();
+    		difference = target_angle - gyro.getAbsoluteAngle();
     	}
     	else {
-    		difference = target_angle - Robot.gyro.getAngle();
+    		difference = target_angle - gyro.getAngle();
     	}
     	//If the angle difference is above THRESHOLD_ANGLE, turn at HIGH_SPEED
     	if (!clockwise && difference > THRESHOLD_ANGLE) {
@@ -75,25 +90,7 @@ public class GyroTurnCommand extends CommandBase {
     	}
     	r = calculated_speed;
     	l = -calculated_speed;
-    	Robot.drivetrain.setPercentVoltage(l, r);
+    	drivetrain.setPercentVoltage(l, r);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	if (isTimedOut()) {
-    		Robot.autonomous_command_group.cancel();
-    	}
-        return finished || isTimedOut();
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    	Robot.drivetrain.setPercentVoltage(0, 0);
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	Robot.drivetrain.setPercentVoltage(0, 0);
-    }
 }
